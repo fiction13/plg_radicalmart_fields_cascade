42cdd7e5c45a212b5b9da6046bdcf92995aa9605
fiction13 <fiction13@yandex.ru>
2023-06-26 21:46:43 +0300

    Reset to dev

--- layouts/administrator/field/cascade.php
+++ layouts/administrator/field/cascade.php
@@ -2,7 +2,7 @@
 /*
  * @package     RadicalMart Fields Standard Plugin
  * @subpackage  plg_radicalmart_fields_standard
- * @version     1.0.0
+ * @version     __DEPLOY_VERSION__
  * @author      Delo Design - delo-design.ru
  * @copyright   Copyright (c) 2022 Delo Design. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
